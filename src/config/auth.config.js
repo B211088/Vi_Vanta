3f49c75596b8b929dbccb822572d082bfd05dcba
Trương Chí Nguyên <139199932+B211088@users.noreply.github.com>
2025-05-23 01:47:19 +0700

    Delete src directory

--- src/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-dotenv.config();
-import dotenv from "dotenv";
-
-const JWT_SECRET = process.env.JWT_SECRET || "34dfsfsdfdsfsdfsdfsdfsdfsdf";
-const JWT_EXPIRATION = "30d";
-
-export { JWT_SECRET, JWT_EXPIRATION };
